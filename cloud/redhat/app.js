92dd04dac9ddcb68cfaf738e48c7ebb76ea4d0d6
davhklee <davhklee@gmail.com>
2017-12-10 10:35:00 -0800

    Implemented data normalization for feature scaling in preprocessing.

---
+++ cloud/redhat/app.js
@@ -0,0 +1,136 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local'),Strategy;
+var mongo = require('mongodb');
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+if (mongoURL == null) {
+  var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
+  // If using plane old env vars via service discovery
+  if (process.env.DATABASE_SERVICE_NAME) {
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
+    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
+    mongoDatabase = process.env[mongoServiceName + '_DATABASE'];
+    mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
+    mongoUser = process.env[mongoServiceName + '_USER'];
+
+  // If using env vars from secret from service binding  
+  } else if (process.env.database_name) {
+    mongoDatabase = process.env.database_name;
+    mongoPassword = process.env.password;
+    mongoUser = process.env.username;
+    var mongoUriParts = process.env.uri && process.env.uri.split("//");
+    if (mongoUriParts.length == 2) {
+      mongoUriParts = mongoUriParts[1].split(":");
+      if (mongoUriParts && mongoUriParts.length == 2) {
+        mongoHost = mongoUriParts[0];
+        mongoPort = mongoUriParts[1];
+      }
+    }
+  }
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+  }
+}
+console.log("database details:");
+console.log(mongoURL);
+console.log(mongoDatabase);
+
+/*
+var mongoose = require('mongoose');
+mongoURL=mongodb://user6UV:WHy4ETFjOSvHs8Aw@172.30.188.161:27017/sampledb
+mongoDatabase=sampledb
+mongoose.connect('mongodb://user6UV:WHy4ETFjOSvHs8Aw@172.30.188.161:27017/sampledb');
+var db = mongoose.connection;
+*/
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+// Init App
+var app = express();
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+	secret: 'secret',
+	saveUninitialized: true,
+	resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+	errorFormatter: function(param, msg, value) {
+		var namespace = param.split('.')
+		, root = namespace.shift()
+		, formParam = root;
+	while(namespace.length) {
+		formParam += '[' + namespace.shift() + ']';
+	}
+	return {
+		param: formParam,
+		msg: msg,
+		value: value
+	};
+	}
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	res.locals.user = req.user || null;
+	next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+// Set Port
+app.set('port', (process.env.PORT || 8080));
+
+app.listen(app.get('port'), function() {
+	console.log('Server started on port ' + app.get('port'));
+});
+
+module.exports = app;
+
