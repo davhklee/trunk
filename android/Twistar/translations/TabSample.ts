b62c09111417834baa2f34dc3bc6dde0ac94ca23
davhklee <davhklee@gmail.com>
2018-04-22 03:15:00 -0700

    update readme

---
+++ android/Twistar/translations/TabSample.ts
@@ -0,0 +1,142 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>TabSample</name>
+    <message>
+        <location filename="../src/TabSample.cpp" line="151"/>
+        <location filename="../src/TabSample.cpp" line="164"/>
+        <source>BBM Connection Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/TabSample.cpp" line="152"/>
+        <location filename="../src/TabSample.cpp" line="165"/>
+        <source>BBM is not currently connected. Please setup / sign-in to BBM to remove this message.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../assets/main.qml" line="61"/>
+        <source>Please enter song name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="156"/>
+        <source>Contacting server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="167"/>
+        <source>No result found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="178"/>
+        <source>Search history cleared</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="201"/>
+        <source>Online</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="208"/>
+        <source>Listening on Twistar: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="225"/>
+        <source>Billboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="234"/>
+        <source>Top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="242"/>
+        <source>Bottom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="250"/>
+        <source>Invite BBM Download</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="262"/>
+        <source>mailto:automechtic@hotmail.com?subject=Twistar%20Technical%20Support</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="264"/>
+        <source>Contact Us</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="22"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="267"/>
+        <source>Clear History</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="298"/>
+        <source>Twistar v1.0.0.3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="301"/>
+        <source>This software is developed, tested and distributed by developer Automechtic.  Automechtic reserves the rights to main application icon, source code and search algorithm contained herein.  2013 All rights reserved.  This software uses a unique search algorithm to locate digital contents on the world wide web.  It does not imply ownership of contents it links to, neither does it own, facilitate or endorse their contents.  For application support, please contact us.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="310"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="321"/>
+        <source>Twistar uses a unique search algorithm to locate songs available on the world wide web.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="326"/>
+        <source>To start searching for your favorite songs, simply enter song name in the text box on top and press ENTER on your keyboard or press the magnifying glass button to the right.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="331"/>
+        <source>To help you navigate through the song list on phones with physical keyboard, use &apos;t&apos; button to move to top of the list, &apos;b&apos; to bottom of the list, &apos;p&apos; to move back by one song and &apos;n&apos; to move forward by one.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="340"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="343"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="349"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="363"/>
+        <source>Display song on BBM status?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
