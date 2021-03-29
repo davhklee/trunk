b7293ba33cd9c0131ee7be2b2435304d55950a13
davhklee <davhklee@gmail.com>
2021-03-29 13:02:00 -0700

    new commit 2976

---
+++ android/Twistar/translations/TabSample_zh_TW.ts
@@ -0,0 +1,142 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_TW">
+<context>
+    <name>TabSample</name>
+    <message>
+        <location filename="../src/TabSample.cpp" line="151"/>
+        <location filename="../src/TabSample.cpp" line="164"/>
+        <source>BBM Connection Error</source>
+        <translation>BBM 通信錯誤</translation>
+    </message>
+    <message>
+        <location filename="../src/TabSample.cpp" line="152"/>
+        <location filename="../src/TabSample.cpp" line="165"/>
+        <source>BBM is not currently connected. Please setup / sign-in to BBM to remove this message.</source>
+        <translation>當前未能連接BBM。請登陸BBM刪除此消息。</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../assets/main.qml" line="61"/>
+        <source>Please enter song name</source>
+        <translation>請輸入歌名</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="156"/>
+        <source>Contacting server</source>
+        <translation>連接服務器中</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="167"/>
+        <source>No result found</source>
+        <translation>沒有搜索結果</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="178"/>
+        <source>Search history cleared</source>
+        <translation>搜索歷史已被清空</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="201"/>
+        <source>Online</source>
+        <translation>在綫</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="208"/>
+        <source>Listening on Twistar: </source>
+        <translation>在音樂旋風收聽: </translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="225"/>
+        <source>Billboard</source>
+        <translation>排行榜</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="234"/>
+        <source>Top</source>
+        <translation>最前</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="242"/>
+        <source>Bottom</source>
+        <translation>最後</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="250"/>
+        <source>Invite BBM Download</source>
+        <translation>邀請 BBM 下載</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="262"/>
+        <source>mailto:automechtic@hotmail.com?subject=Twistar%20Technical%20Support</source>
+        <translation>mailto:automechtic@hotmail.com?subject=音樂旋風技術支持</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="264"/>
+        <source>Contact Us</source>
+        <translation>聯係我們</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="22"/>
+        <source>About</source>
+        <translation>關於</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="267"/>
+        <source>Clear History</source>
+        <translation>清空歷史</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="298"/>
+        <source>Twistar v1.0.0.3</source>
+        <translation>音樂旋風 v1.0.0.3</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="301"/>
+        <source>This software is developed, tested and distributed by developer Automechtic.  Automechtic reserves the rights to main application icon, source code and search algorithm contained herein.  2013 All rights reserved.  This software uses a unique search algorithm to locate digital contents on the world wide web.  It does not imply ownership of contents it links to, neither does it own, facilitate or endorse their contents.  For application support, please contact us.</source>
+        <translation>此軟件的開發，測試和發布權均歸開發者Automechtic所有。 Automechtic擁有主應用程序圖標，源代碼和搜索引擎的版權和其他相關知識產權。 2013所有權利予以保留。該軟件採用獨特的搜索引擎來用以定位互聯網上的數碼內容。這並不表示著搜索連接到內容的所有權或是認可權屬於該軟件。如您需要任何協助，請聯係我們。</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="310"/>
+        <source>Help</source>
+        <translation>幫助</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="321"/>
+        <source>Twistar uses a unique search algorithm to locate songs available on the world wide web.</source>
+        <translation>音樂旋風采用了獨特的搜索方法從互聯網上搜索歌曲。</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="326"/>
+        <source>To start searching for your favorite songs, simply enter song name in the text box on top and press ENTER on your keyboard or press the magnifying glass button to the right.</source>
+        <translation>開始搜索您最喜愛的歌曲，只需在上面的文本框中輸入歌曲名稱，然後按確定鍵或按右側放大鏡按鈕。</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="331"/>
+        <source>To help you navigate through the song list on phones with physical keyboard, use &apos;t&apos; button to move to top of the list, &apos;b&apos; to bottom of the list, &apos;p&apos; to move back by one song and &apos;n&apos; to move forward by one.</source>
+        <translation>為了讓用戶能夠在鍵盤手機上更容易瀏覽歌曲列表，你可以使用&apos;t&apos;按鈕移動到列表頂部，&apos;b&apos;按鈕移動到底部,&apos;p&apos;按鈕回前一首歌和&apos;n&apos;往後移一首歌。</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="340"/>
+        <source>Settings</source>
+        <translation>設置</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="343"/>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="349"/>
+        <source>Save</source>
+        <translation>儲存</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="363"/>
+        <source>Display song on BBM status?</source>
+        <translation>在 BBM 顯示歌曲</translation>
+    </message>
+</context>
+</TS>
